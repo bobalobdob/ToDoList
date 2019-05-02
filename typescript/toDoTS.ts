20ed6869634758d7a0f0e43b72638125e746075d
KYRAN Perdue <kyranperdue@gmail.com>
2019-05-02 14:43:18 -0700

    added documentation for future typescript plans and added the getItemFromForm funciton
    changed the HTML to match

--- typescript/toDoTS.ts
+++ typescript/toDoTS.ts
@@ -6,3 +6,44 @@ class ToDoItem{
     isComplete:boolean;
     urgency:string;
 }
+
+//when add item is clicked
+    //get data off page and wrap in todo object
+    //notify user and clear form
+    //the todo object slash list
+
+window.onload = function(){
+    let addBtn = <HTMLButtonElement>document.querySelector("#create-item > button");
+    addBtn.onclick = processNewItem;
+}
+
+function processNewItem(){
+    let item:ToDoItem = getItemFromForm();
+    saveItem(item);
+    notifyUser();
+    clearForm();
+}
+
+/**
+ * Get all user input from form
+ * and wrap it in a ToDoItem
+ */
+function getItemFromForm(){
+    let item = new ToDoItem();
+    item.title = (<HTMLInputElement>document.getElementById("title")).value;
+
+    item.description = (<HTMLInputElement>document.getElementById("description")).value;
+
+    let itemStartDate:string = (<HTMLInputElement>document.getElementById("start-date")).value;
+    item.startDate = new Date(itemStartDate);
+
+    let itemEndDate:string = (<HTMLInputElement>document.getElementById("start-date")).value;
+    item.endDate = new Date(itemEndDate);
+
+    item.isComplete = (<HTMLInputElement>document.getElementById("is-complete")).checked;
+
+    let urgencyElem = <HTMLSelectElement>document.getElementById("urgency");
+    item.urgency = urgencyElem.options[urgencyElem.selectedIndex].value
+
+    return item;
+}
